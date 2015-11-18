899aa97a9913d673f1d9d93717f434143a10146e
Sage Rimal <srimal@campusexplorer.com>
2015-11-17 16:15:41 -0800

    some changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,12 +28,12 @@ class EventsController < ApplicationController
     selected_hour = params[:time]["time(4i)"].to_s
     selected_minute = params[:time]["time(5i)"].to_s
 
-    puts "HEY GUYS" 
+    puts "HEY GUYS"
     puts selected_minute, selected_hour
     puts "HEY GUYS"
     puts "HEY GUYS"
 
-    @event["time"] = selected_hour.to_s+":"+selected_minute.to_s
+    @event["time"] = "#{selected_hour}:#{selected_minute}".to_s
 
     user = current_user[:id]
     @event.user_id= user
