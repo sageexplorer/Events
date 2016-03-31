bd320ade41566748227a63fd30ea8cb43d751ebf
Sage Rimal <srimal@campusexplorer.com>
2016-03-31 14:54:56 -0700

    new rules

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -63,21 +63,21 @@ class EventsController < ApplicationController
     time = Time.new
     time = time.strftime("%H:%M")
     @message.each do |t|
-       t[:time].strftime("%H:%M") == time
+      if t[:time].strftime("%H:%M") == time
 
-      puts t[:time].strftime("%H:%M")
-      #Time spread contains time from not till 10 minutes from now
-      i = 0
-      v = 0
-      time_spread = []
-      while i < 15 do
-        v = v + 60
-        time_ = Time.now + v
-        i+=1
-        time_spread.push(time_.strftime("%H:%M"))
-      end
-      #If time from now till 10 minutes from now contains the time in the database
-      if time_spread.include? (t[:time].strftime("%H:%M"))
+        puts t[:time].strftime("%H:%M")
+        #Time spread contains time from not till 10 minutes from now
+        #i = 0
+        #v = 0
+        #time_spread = []
+        #while i < 15 do
+        #  v = v + 60
+        #  time_ = Time.now + v
+        #  i+=1
+        #  time_spread.push(time_.strftime("%H:%M"))
+        #end
+        ##If time from now till 10 minutes from now contains the time in the database
+        #if time_spread.include? (t[:time].strftime("%H:%M"))
 
         message= t["suggest"]
         phone = t["place"]
